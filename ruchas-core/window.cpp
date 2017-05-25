f4ad89946c235cdcce286c3db74840d93fc9acd5
david <david.maseda@hotmail.com>
2017-05-25 19:45:51 +0200

    Added GLEW submodule

---
+++ ruchas-core/window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 25/05/17.
+//
+
+#include "window.h"
---
+++ ruchas-core/window.h
@@ -0,0 +1,14 @@
+//
+// Created by david on 25/05/17.
+//
+
+#ifndef RUCHASENGINE_WINDOW_H
+#define RUCHASENGINE_WINDOW_H
+
+
+class window {
+
+};
+
+
+#endif //RUCHASENGINE_WINDOW_H
